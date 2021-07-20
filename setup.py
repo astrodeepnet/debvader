c2d10b11345dd1f1d48e5900b79c996c445d83ec
Bastien Arcelin <barcelin@cca007.in2p3.fr>
2021-07-20 12:19:44 +0200

    update package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'debvader',
+    url = 'https://github.com/BastienArcelin/debvader',
+    author = 'Bastien Arcelin, Cyrille Doux, Biswajit Biswas',
+    author_email = 'arcelin@apc.in2p3.fr',
+    packages=['debvader'],
+    install_requires=['numpy'],
+    version='0.1',
+    description="Galaxy deblender from variational autoencoders"
+    )
