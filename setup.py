1989935980be0cbed934c02b4aafc52c85473a6e
Bastien Arcelin <barcelin@cca008.in2p3.fr>
2021-07-22 19:35:31 +0200

    create package files

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+# setup(
+#     name = 'debvader',
+#     url = 'https://github.com/BastienArcelin/debvader',
+#     author = 'Bastien Arcelin, Cyrille Doux, Biswajit Biswas',
+#     author_email = 'arcelin@apc.in2p3.fr',
+#     packages=['debvader'],
+#     install_requires=['numpy'],
+#     version='0.1',
+#     description="Galaxy deblender from variational autoencoders",
+#     # long_description=open('README.txt').read(),
+#     )
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["numpy","tensorflow"]
 
 setup(
-    name = 'debvader',
-    url = 'https://github.com/BastienArcelin/debvader',
-    author = 'Bastien Arcelin, Cyrille Doux, Biswajit Biswas',
-    author_email = 'arcelin@apc.in2p3.fr',
-    packages=['debvader'],
-    install_requires=['numpy'],
-    version='0.1',
+    name="debvader",
+    version="0.0.1",
+    author="Bastien Arcelin, Cyrille Doux, Biswajit Biswas",
+    author_email="arcelin@apc.in2p3.fr",
     description="Galaxy deblender from variational autoencoders",
-    # long_description=open('README.txt').read(),
-    )
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/BastienArcelin/debvader",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+    ],
+)
