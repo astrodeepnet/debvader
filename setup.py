59865cbb31722cc4c5fb8df36f34502c3530c300
Bastien Arcelin <barcelin@cca008.in2p3.fr>
2021-07-22 21:23:47 +0200

    test

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy>=1.11","tensorflow==2.1.0","tensorflow-probability==0.9.0"]
+requirements = ["numpy==1.17.4","tensorflow==2.1.0","tensorflow-probability==0.9.0"]
 
 setup(
     name="debvader",
