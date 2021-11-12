1295ec8b9b00c5106fd0369649178d62ecd622d7
Bastien Arcelin <barcelin@cca007.in2p3.fr>
2021-11-12 16:27:34 +0100

    update notebook, weights of network for unnormalised images, change model to output a distribution
    Former-commit-id: be4c1a41afbc2389478eb5a96eea16af8eaca330

--- debvader/debvader.py
+++ debvader/debvader.py
@@ -5,12 +5,17 @@ import tensorflow as tf
 import tensorflow.keras.backend as K
 
 path_folder = os.path.dirname(os.path.abspath(__file__))
+print(path_folder)
+#import debvader
+#from debvader import model
 
-import debvader
-from debvader import model
+###### TO SUPRESSS AND UNCOMMENT PREVIOUS LINES
+import sys
+sys.path.insert(0,'.')
+import model
+######
 
-
-def load_deblender(survey, input_shape, latent_dim, filters, kernels):
+def load_deblender(survey, input_shape, latent_dim, filters, kernels, return_encoder_decoder = False):
     """
     load weights trained for a particular dataset
     parameters:
@@ -21,7 +26,7 @@ def load_deblender(survey, input_shape, latent_dim, filters, kernels):
         kernels: kernels used for the convolutional layers
     """
     # Create the model
-    net = model.create_model_vae(
+    net, encoder, decoder = model.create_model_vae_2(
         input_shape,
         latent_dim,
         filters,
@@ -32,10 +37,13 @@ def load_deblender(survey, input_shape, latent_dim, filters, kernels):
 
     # Define the loss function
     def vae_loss(x, x_decoded_mean):
-        xent_loss = K.mean(
-            K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=[1, 2, 3])
-        )
-        return xent_loss
+        #xent_loss = K.mean(
+        #    K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=[1, 2, 3])
+        #)
+        return -x_decoded_mean.log_prob(x)#xent_loss
+
+    # Set the decoder as non-trainable
+    decoder.trainable = False
 
     # Compile the model
     net.compile(
@@ -45,11 +53,15 @@ def load_deblender(survey, input_shape, latent_dim, filters, kernels):
     )
 
     # Load the weights corresponding to the chosen survey
-    loading_path = str(path_folder) + "/data/weights/" + survey + "/"
+    loading_path = str(path_folder) + "/../data/weights/" + survey + "/not_normalised/"
     latest = tf.train.latest_checkpoint(loading_path)
     net.load_weights(latest)
 
-    return net
+
+    if return_encoder_decoder:
+        return net, encoder, decoder
+    else:
+        return net
 
 
 def deblend(net, images):
@@ -60,9 +72,9 @@ def deblend(net, images):
         images: array of images. It can contain only one image.
     """
     # Normalize the images
-    images_normed = np.tanh(np.arcsinh(images))
+    #images_normed = np.tanh(np.arcsinh(images))
 
     # Denorm deblended images
-    images_deblended = np.sinh(np.arctanh(net.predict(images_normed)))
+    #images_deblended = np.sinh(np.arctanh(net.predict(images_normed)))
 
-    return images_deblended
+    return net(tf.cast(images, tf.float32)).mean().numpy(), net(tf.cast(images, tf.float32))
--- debvader/model.py
+++ debvader/model.py
@@ -111,3 +111,152 @@ def create_model_vae(
         net.layers[22 + i].trainable = False
 
     return net
+
+
+def create_encoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+):
+    # Define the prior for the latent space
+    prior = tfd.Independent(
+        tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
+    )
+
+    # Input layer
+    input_layer = Input(shape=(input_shape))
+
+    # Define the model
+    h = BatchNormalization()(input_layer)
+    for i in range(len(filters)):
+        h = Conv2D(
+            filters[i], (kernels[i], kernels[i]), activation=None, padding="same"
+        )(h)
+        h = PReLU()(h)
+        h = Conv2D(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=None,
+            padding="same",
+            strides=(2, 2),
+        )(h)
+        h = PReLU()(h)
+
+    h = Flatten()(h)
+    h = PReLU()(h)
+    h = Dense(
+        tfp.layers.MultivariateNormalTriL.params_size(latent_dim), activation=None
+    )(h)
+
+    return Model(input_layer, h)
+
+
+def create_decoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+):
+
+    input_layer = Input(shape=(latent_dim,))
+    h = PReLU()(input_layer)
+    h = Dense(tfp.layers.MultivariateNormalTriL.params_size(32))(h)
+    h = PReLU()(h)
+    w = int(np.ceil(input_shape[0] / 2 ** (len(filters))))
+    h = Dense(w * w * filters[-1], activation=dense_activation)(tf.cast(h, tf.float32))
+    h = PReLU()(h)
+    h = Reshape((w, w, filters[-1]))(h)
+    for i in range(len(filters) - 1, -1, -1):
+        h = Conv2DTranspose(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=conv_activation,
+            padding="same",
+            strides=(2, 2),
+        )(h)
+        h = PReLU()(h)
+        h = Conv2DTranspose(
+            filters[i],
+            (kernels[i], kernels[i]),
+            activation=conv_activation,
+            padding="same",
+        )(h)
+        h = PReLU()(h)
+        
+    h = Conv2D(input_shape[-1]*2, (3, 3), activation="relu", padding="same")(h)
+
+    # In case the last convolutional layer does not provide an image of the size of the input image, cropp it.
+    cropping = int(h.get_shape()[1] - input_shape[0])
+    if cropping > 0:
+        print("in cropping")
+        if cropping % 2 == 0:
+            h = Cropping2D(cropping / 2)(h)
+        else:
+            h = Cropping2D(
+                ((cropping // 2, cropping // 2 + 1), (cropping // 2, cropping // 2 + 1))
+            )(h)
+
+    # Build the encoder only
+    h = tfp.layers.DistributionLambda(make_distribution_fn=lambda t: tfd.Normal(loc=t[...,:6], scale=1e-4 +t[...,6:])
+                                          ,convert_to_tensor_fn=tfp.distributions.Distribution.sample)(h)
+    
+    
+    
+    return Model(input_layer,h)
+
+def create_model_vae_2(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+):
+    """
+    Create the VAE model
+    parameters:
+        input_shape: shape of input tensor
+        latent_dim: size of the latent space
+        filters: filters used for the convolutional layers
+        kernels: kernels used for the convolutional layers
+    """
+    
+    encoder = create_encoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+)
+    
+    decoder = create_decoder(
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+)
+    print("we are here")
+
+    # Define the prior for the latent space
+    prior = tfd.Independent(
+        tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
+    )
+
+    # Build the model
+    x_input = Input(shape=(input_shape))
+    z = tfp.layers.MultivariateNormalTriL(
+        latent_dim,
+        activity_regularizer=tfp.layers.KLDivergenceRegularizer(prior, weight=0.01),
+    )(encoder(x_input))
+    
+    net = Model(inputs = x_input, outputs = decoder(z))
+    
+    return net, encoder, decoder
