f39459411d1e0e41b8e1ddaa196e7813ac38f03f
Bastien Arcelin <barcelin@cca004.in2p3.fr>
2021-12-03 16:36:23 +0100

    remove a print in load_deblender

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requirements = [
 
 setup(
     name="debvader",
-    version="0.0.812",
+    version="0.0.8121",
     author="Bastien Arcelin, Cyrille Doux, Thomas Sainrat, Biswajit Biswas, Alexandre Boucaud",
     author_email="arcelin@apc.in2p3.fr",
     description="Galaxy deblender from variational autoencoders",
