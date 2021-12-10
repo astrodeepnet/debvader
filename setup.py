cbadb941d1b00735c71af41c60aaf06a8f362384
b-biswas <bbiswas.dgp@gmail.com>
2021-12-10 14:27:44 +0100

    removed data from packages

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/BastienArcelin/debvader",
     include_package_data=True,
-    packages=['data','debvader'],
+    packages=['debvader'],
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.6",
