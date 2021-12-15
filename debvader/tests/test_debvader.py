31d5423e3d0c01d13e32adbf4f099666646c1c7f
Bastien Arcelin <43615932+BastienArcelin@users.noreply.github.com>
2021-12-15 09:16:19 +0100

    Update test_debvader.py
    suppressed unnecessary lines and use of field_size arguement in extract_cutouts function

--- debvader/tests/test_debvader.py
+++ debvader/tests/test_debvader.py
@@ -3,23 +3,22 @@ import numpy as np
 
 def test_cutouts_border():
     
-    field_size=15
+    field_size = 15
     nb_of_bands = 3
+    cutout_size = 5
 
     image = np.random.rand(1, field_size, field_size, nb_of_bands)
     galaxy_distances_to_center = [[5,5]]
-    cutout_size = 5
-
-    cutout = extract_cutouts(field_image=image.copy(), field_size=15, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
+    
+    cutout = extract_cutouts(field_image=image.copy(), field_size=field_size, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
 
     assert cutout[0][0][1][2][0] == image[0][11][12][0]
     assert cutout[0][0][4][4][2] == image[0][14][14][2]
     assert cutout[0][0][2][3][1] == image[0][12][13][1]
 
     galaxy_distances_to_center = [[-5,-5]]
-    cutout_size = 5
 
-    cutout = extract_cutouts(field_image=image.copy(), field_size=15, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
+    cutout = extract_cutouts(field_image=image.copy(), field_size=field_size, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
 
     assert cutout[0][0][4][4][0] == image[0][4][4][0]
     assert cutout[0][0][2][3][1] == image[0][2][3][1]
@@ -28,8 +27,7 @@ def test_cutouts_border():
     # test when cutout is too large
     
     galaxy_distances_to_center = [[6,6]]
-    cutout_size = 5
 
-    cutout = extract_cutouts(field_image=image.copy(), field_size=15, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
+    cutout = extract_cutouts(field_image=image.copy(), field_size=field_size, galaxy_distances_to_center=galaxy_distances_to_center, cutout_size=cutout_size, nb_of_bands=nb_of_bands)
 
     assert len(cutout[1]) == 0
