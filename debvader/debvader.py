f58a7d239ca445009c024aa17225859db26be365
b-biswas <bbiswas.dgp@gmail.com>
2022-02-08 20:22:37 +0100

    np.recarray and code readability

--- debvader/debvader.py
+++ debvader/debvader.py
@@ -13,6 +13,7 @@ import sep
 
 from astropy.table import vstack, Table
 from debvader import model
+import pandas as pd
 
 def extract_cutouts(field_image, field_size, galaxy_distances_to_center,cutout_size=59, nb_of_bands = 6):
     """
@@ -48,6 +49,8 @@ def extract_cutouts(field_image, field_size, galaxy_distances_to_center,cutout_s
         print("Some galaxies are too close from the border of the field to be considered here.")
             
     return cutout_images, list_idx
+
+
 def detect_objects(field_image):
         '''
         Detect the objects in the field_image image using the SExtractor detection algorithm.
@@ -188,11 +191,22 @@ def position_optimization(field_image, output_image_mean_padded, galaxy_distance
 
 class DeblendField:
 
-    def __init__(self, net, field_image, cutout_images=None, cutout_size=59, nb_of_bands=6, epistemic_uncertainty_estimation=False, normalised=False):
+    def __init__(self, net, field_image, cutout_size=59, nb_of_bands=6, epistemic_uncertainty_estimation=False, normalised=False):
+        """
+        to initialize
+
+        parameters:
+            net: network used to deblend the field
+            field_image: image of the field to deblend
+            cutout_size: size of the stamps
+            nb_of_bands: number of filters in the image
+            epistemic_uncertainty_estimation: boolean to indication if expestemic uncertainity extimation is to be done.
+            normalised: boolean to indicate if images need to be normalised
+        """
+        
         self.net = net 
         self.field_image = field_image.copy()
         self.field_size = field_image.shape[1]
-        self.cutout_images = cutout_images
         self.cutout_size = cutout_size
         self.nb_of_bands = nb_of_bands
         self.epistemic_uncertainty_estimation = epistemic_uncertainty_estimation
@@ -203,6 +217,17 @@ class DeblendField:
         self.mse = []
 
     def get_residual_field(self, res_deblend=None):
+        """
+        Calculates the residual field 
+
+        parameters: 
+            res_deblend: astropy.table.Table that takes as input the result of deblending. 
+                if left as None, it will automatically use the output of deblend_field 
+                or iterative_deblending function
+        returns: 
+            deblended_image: residual image after substracting the predicted deblended images.
+                dimensions are the same as the input field.
+        """
 
         if res_deblend is None:
             res_deblend = self.res_deblend
@@ -212,9 +237,12 @@ class DeblendField:
             for isolated_galaxy_row in res_deblend:
 
                 # First create padded images of the stamps at the size of the field to allow for a simple subtraction.
+
+                # TODO: may be use some inbuild padding function?
                 output_images_mean_padded = np.zeros((1, self.field_size, self.field_size, self.nb_of_bands))
-                output_images_mean_padded[:,int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),
-                                    int((self.field_size - self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),:]=isolated_galaxy_row['output_images_mean']
+                pos_offset = int((self.field_size-self.cutout_size)/2)
+
+                output_images_mean_padded[:,pos_offset:self.cutout_size+pos_offset, pos_offset:self.cutout_size+pos_offset,:] = isolated_galaxy_row['output_images_mean']
                                     
                 x_pos = isolated_galaxy_row['galaxy_distances_to_center_x'] + isolated_galaxy_row['shifts'][0]
                 y_pos = isolated_galaxy_row['galaxy_distances_to_center_y'] + isolated_galaxy_row['shifts'][1]
@@ -224,7 +252,16 @@ class DeblendField:
         return deblended_image
 
     def get_predicted_field(self, res_deblend=None):
+        """
+        Calculates the predicted mean field, predicted stddev field and epistemic uncertainity field.
 
+        parameters: 
+            res_deblend: astropy.table.Table that takes as input the result of deblending. 
+                if left as None, it will automatically use the output of deblend_field or iterative_deblending functions
+
+        returns: 
+            predicted_field: dictionary with keys - 'predicted_mean_field', 'predicted_stddev_field' and 'predicted_epistemic_field'
+        """
         if res_deblend is None:
             res_deblend = self.res_deblend
 
@@ -242,22 +279,21 @@ class DeblendField:
                 epistemic_uncertainty = isolated_galaxy_row['epistemic_uncertainty']
                 
                 # First create padded images of the stamps at the size of the field to allow for a simple subtraction.
+
+                pad_start = int((self.field_size-self.cutout_size)/2)
+                pad_end = self.cutout_size+int((self.field_size-self.cutout_size)/2)
+
                 output_images_mean_padded = np.zeros((self.field_size, self.field_size, self.nb_of_bands))
-                output_images_mean_padded[int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),
-                                    int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),:]=output_images_mean
+                output_images_mean_padded[pad_start:pad_end, pad_start:pad_end, :] = output_images_mean
 
                 # Create the corresponding standard deviation image (aleatoric uncertainty).
                 output_images_stddev_padded = np.zeros((self.field_size, self.field_size, self.nb_of_bands))
-                output_images_stddev_padded[int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),
-                                    int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),:]=output_images_stddev
-
-                
-
+                output_images_stddev_padded[pad_start:pad_end, pad_start:pad_end, :] = output_images_stddev
 
                 x_pos = isolated_galaxy_row['galaxy_distances_to_center_x'] + isolated_galaxy_row['shifts'][0]
                 y_pos = isolated_galaxy_row['galaxy_distances_to_center_y'] + isolated_galaxy_row['shifts'][1]
 
-                for band in range(self.nb_of_bands):
+                for band in range(self.nb_of_bands): # scipy.ndimage.shift is slow so looping over bands is faster
 
                     denoised_field[:, :, band] += scipy.ndimage.shift(output_images_mean_padded[:, :, band], shift=(x_pos, y_pos))
                     denoised_field_std[:, :, band] += scipy.ndimage.shift(output_images_stddev_padded[:, :, band], shift=(x_pos, y_pos))
@@ -265,45 +301,55 @@ class DeblendField:
                     output_images_epistemic_padded = np.zeros((self.field_size,self.field_size,self.nb_of_bands))
                     if self.epistemic_uncertainty_estimation:
                         # Create the corresponding epistemic uncertainty image (aleatoric uncertainty).
-                        output_images_epistemic_padded[int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),
-                                            int((self.field_size-self.cutout_size)/2):self.cutout_size+int((self.field_size-self.cutout_size)/2),:]=np.array(epistemic_uncertainty)
+                        output_images_epistemic_padded[pad_start:pad_end, pad_start:pad_end, :] = np.array(epistemic_uncertainty)
                         denoised_field_epistemic[:, :, band] += scipy.ndimage.shift(output_images_epistemic_padded[:, :, band], shift=(x_pos, y_pos))
 
-                
-
         predicted_field = {}
-        predicted_field['denoised_field'] = denoised_field
-        predicted_field['denoised_field_std'] = denoised_field_std
-        predicted_field['denoised_field_epistemic'] = denoised_field_epistemic
+        predicted_field['predicted_mean_field'] = denoised_field
+        predicted_field['predicted_stddev_field'] = denoised_field_std
+        predicted_field['predicted_epistemic_field'] = denoised_field_epistemic
 
         return predicted_field
 
     def get_deblending_meta_data(self, res_deblend=None):
+        """
+        function to compute: the residual image, predicted mean field, preicted stddev field and predicted_epistemic_field at the same time
 
+        parameters: 
+            res_deblend: astropy.table.Table that takes as input the result of deblending. 
+                if left as None, it will automatically use the output of deblend_field or iterative_deblending functions
+
+        returns: 
+            predicted_field: dictionary with keys - 'field_image', 'delended_image', 'predicted_mean_field', 'predicted_stddev_field' 
+                and 'predicted_epistemic_field'
+        """
         res_deblend_meta = {}
         res_deblend_meta['field_image'] = self.field_image
         res_deblend_meta['deblended_image'] = self.get_residual_field(res_deblend)
         predicted_field = self.get_predicted_field(res_deblend)
-        res_deblend_meta['denoised_field']  = predicted_field['denoised_field']
-        res_deblend_meta['denoised_field_std'] = predicted_field['denoised_field_std']
-        res_deblend_meta['denoised_field_epistemic'] = predicted_field['denoised_field_epistemic']
+        res_deblend_meta['predicted_mean_field']  = predicted_field['predicted_mean_field']
+        res_deblend_meta['predicted_stddev_field'] = predicted_field['predicted_stddev_field']
+        res_deblend_meta['predicted_epistemic_field'] = predicted_field['predicted_epistemic_field']
 
         return res_deblend_meta
 
-    def deblend_field(self, galaxy_distances_to_center, field_image=None, cutout_images = None, optimise_positions=False, epistemic_criterion=100., mse_criterion=100.):
+    def deblend_field(self, galaxy_distances_to_center, cutout_images = None, optimise_positions=False, epistemic_criterion=100., mse_criterion=100., field_image=None):
         """
-        Deblend a field of galaxies
+        Deblend a field of galaxies it returns 
         parameters:
-            net: network used to deblend the field
-            field_image: image of the field to deblend
             galaxy_distances_to_center: distances of the galaxies to deblend from the center of the field. In pixels.
             cutout_images: stamps centered on the galaxies to deblend
-            cutout_size: size of the stamps
-            nb_of_bands: number of filters in the image
             optimise_position: boolean to indicate if the user wants to use the scipy optimize package to optimise the position of the galaxy
-            normalised: boolean to indicate if images need to be normalised
+            epistemic_criterion: cut for epistemic uncertainity to get rid of bad predictions
+            mse_criterion: cut for mse_criterion to get rid of bad predictions
+            field_image: image of the field to deblend. Recommended to leave as None, by default it will use self.field_image.
+                Note that providing a field other than self.field_image may lead to incorrect results on the residual image.
+
+        returns: 
+            self.res_deblended: np.recarray containing the following for each detected galaxy - 
+                cutout_images, output_images_mean, output_images_stddev, shifts, list_idx, galaxy_distances_to_center_x, galaxy_distances_to_center_y
         """
-        field_size = self.field_image.shape[1]
+        
 
         res_deblend = dict()
         res_deblend['cutout_images']=None
@@ -316,6 +362,7 @@ class DeblendField:
             field_image = self.field_image.copy()
 
         field_size = field_image.shape[1]
+
         # Deblend the cutouts around the detected galaxies. If needed, create the cutouts.
         if isinstance(cutout_images, np.ndarray):
             output_images_mean, output_images_distribution = deblend(self.net, cutout_images, normalised=self.normalised)
@@ -388,7 +435,7 @@ class DeblendField:
         res_deblend['epistemic_uncertainty'] = epistemic_uncertainty
         res_deblend['passed_cuts'] = passed_cuts
 
-        self.res_deblend = Table(res_deblend)
+        self.res_deblend = pd.DataFrame(res_deblend).to_records(index=False)
 
         return self.res_deblend
 
@@ -398,13 +445,11 @@ class DeblendField:
         '''
         Do the iterative deblending of a scene
         paramters:
-            net: network used to deblend the field
-            field_image: image of the field to deblend
             galaxy_distances_to_center: distances of the galaxies to deblend from the center of the field. In pixels.
             cutout_images: stamps centered on the galaxies to deblend
-            cutout_size: size of the stamps
-            nb_of_bands: number of filters in the image
             optimise_position: boolean to indicate if the user wants to use the scipy optimize package to optimise the position of the galaxy
+            epistemic_criterion: cut for epistemic uncertainity to get rid of bad predictions
+            mse_criterion: cut for mse_criterion to get rid of bad predictions
             normalised: boolean to indicate if images need to be normalised
         '''
 
@@ -430,14 +475,14 @@ class DeblendField:
 
             new_residual_field = self.get_residual_field()
 
-            self.mse += [metrics.mean_squared_error(prev_residual_field, new_residual_field)] #todo: prev_residual or original field?
+            self.mse += [metrics.mean_squared_error(prev_residual_field, new_residual_field)]
             #field_img_save, field_image, denoised_field, denoised_field_std, denoised_field_epistemic, cutout_images, output_images_mean, output_images_distribution, shifts, galaxy_distances_to_center, mse_step = deblending_step(net, field_img_init, detection_up_to_k, cutout_images = None, cutout_size = cutout_size, nb_of_bands = nb_of_bands, optimise_positions=optimise_positions, epistemic_uncertainty_estimation=epistemic_uncertainty_estimation, epistemic_criterion=epistemic_criterion, mse_criterion=mse_criterion, normalised=normalised)
             #field_img_init=field_img_save.copy()
 
             if res_step["list_idx"] is None:
                 break
 
-            res_deblend = vstack([res_deblend, res_step])
+            res_deblend = np.concatenate([res_deblend, res_step])
             k+=1
 
             print(f'{sum(self.nb_of_deblended_galaxies)} galaxies found up to this step.')
@@ -452,16 +497,14 @@ class DeblendField:
 
     def deblending_step(self, field_image, cutout_images = None, optimise_positions=False, epistemic_criterion=100., mse_criterion=100.):
         '''
-        One step of the iterative procedure
+        One step of the iterative procedure called within iterative_procedure.
+
         paramters:
-            net: network used to deblend the field
             field_image: image of the field to deblend
-            galaxy_distances_to_center: distances of the galaxies to deblend from the center of the field. In pixels. #TODO: this description is probably wrong 
             cutout_images: stamps centered on the galaxies to deblend
-            cutout_size: size of the stamps
-            nb_of_bands: number of filters in the image
             optimise_position: boolean to indicate if the user wants to use the scipy optimize package to optimise the position of the galaxy
-            normalised: boolean to indicate if images need to be normalised
+            epistemic_criterion: cut for epistemic uncertainity to get rid of bad predictions
+            mse_criterion: cut for mse_criterion to get rid of bad predictions
         '''
         detection_k = detect_objects(field_image)
         # Avoid to have several detection at the same location
