a7126c5c625620f1e35b375bc60d2b4688629280
b-biswas <bbiswas.dgp@gmail.com>
2022-02-20 01:07:46 +0100

    example of data generator

---
+++ debvader/batch_generator.py
@@ -0,0 +1,73 @@
+from random import choice
+
+import numpy as np
+from tensorflow.keras.utils import Sequence
+
+
+class COSMOSsequence(Sequence):
+    def __init__(
+        self,
+        list_of_samples,
+        x_col_name,
+        y_col_name,
+        batch_size,
+        num_iterations_per_epoch,
+        normalization_func,
+        channel_last=False,
+    ):
+        """
+        initializes the Data generator
+
+        parameters:
+        list_of_samples: list of paths to the datafiles.
+        x_col_name: column name of data to be fed as input to the network
+        y_col_name: column name of data to be fed as target to the network
+        batch_size: sample sixe for each batch
+        num_iterations_per_epoch: number of samples (of size = batch_size) to be drawn from the sample
+        normalization_func: function to be used for normalization
+        channel_last: boolean to indicate if the the clast channel corresponds to differnet bands of the input data.
+        """
+        self.list_of_samples = list_of_samples
+        self.x_col_name = x_col_name
+        self.y_col_name = y_col_name
+        self.batch_size = batch_size
+        self.num_iterations_per_epoch = num_iterations_per_epoch
+        self.normalization_func = normalization_func
+        self.channel_last = channel_last
+
+    def __len__(self):
+        return self.num_iterations_per_epoch
+
+    def __getitem__(self, idx):  # WHY idx??
+
+        current_loop_file_name = choice(self.list_of_samples)
+        current_sample = np.load(current_loop_file_name, allow_pickle=True)
+
+        batch = np.random.choice(current_sample, size=self.batch_size, replace=False)
+        x = batch[self.x_col_name]
+        y = batch[self.y_col_name]
+
+        x = np.array(x.tolist())
+        y = np.array(y.tolist())
+
+        x = self.normalization_func(x)
+        y = self.normalization_func(y)
+
+        #  flip : flipping the image array
+        # if not self.channel_last:
+        #    rand = np.random.randint(4)
+        #    if rand == 1:
+        #        x = np.flip(x, axis=-1)
+        #        y = np.flip(y, axis=-1)
+        #    elif rand == 2 :
+        #        x = np.swapaxes(x, -1, -2)
+        #        y = np.swapaxes(y, -1, -2)
+        #    elif rand == 3:
+        #        x = np.swapaxes(np.flip(x, axis=-1), -1, -2)
+        #        y = np.swapaxes(np.flip(y, axis=-1), -1, -2)
+
+        # Change the shape of inputs and targets to feed the network
+        x = np.transpose(x, axes=(0, 2, 3, 1))
+        y = np.transpose(y, axes=(0, 2, 3, 1))
+
+        return x, y
---
+++ debvader/normalize.py
@@ -0,0 +1,8 @@
+def linear_normalization_cosmos(x):
+    """
+    linear normalization used for cosmos dataset
+
+    parameters:
+        x: numpy array to be normalzied.
+    """
+    return x / 80000
