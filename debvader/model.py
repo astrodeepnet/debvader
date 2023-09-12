0f491b5e517cf3b2c7e03c6de11a09a410ee8e82
b-biswas <bbiswas.dgp@gmail.com>
2023-09-12 12:39:10 +0200

    revert to old model

--- debvader/model.py
+++ debvader/model.py
@@ -1,4 +1,4 @@
-import os
+import os 
 
 import numpy as np
 import pkg_resources
@@ -27,6 +27,8 @@ def create_encoder(
     latent_dim,
     filters,
     kernels,
+    conv_activation=None,
+    dense_activation=None,
 ):
     """
     Create the encoder of VAE model
@@ -36,8 +38,14 @@ def create_encoder(
         latent_dim: size of the latent space
         filters: filters used for the convolutional layers
         kernels: kernels used for the convolutional layers
+        conv_activation: activation for conv layers default PReLU
+        desnse_activation: activation for dense layers default PReLU
     """
 
+    # Define the prior for the latent space
+    prior = tfd.Independent(
+        tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
+    )
     # Input layer
     input_layer = Input(shape=(input_shape))
 
@@ -45,10 +53,7 @@ def create_encoder(
     h = BatchNormalization()(input_layer)
     for i in range(len(filters)):
         h = Conv2D(
-            filters[i],
-            (kernels[i], kernels[i]),
-            activation=None,
-            padding="same",
+            filters[i], (kernels[i], kernels[i]), activation=None, padding="same"
         )(h)
         h = PReLU()(h)
         h = Conv2D(
@@ -62,14 +67,11 @@ def create_encoder(
 
     h = Flatten()(h)
     h = PReLU()(h)
-    h = Dense(1024)(h)
-    h = PReLU()(h)
     h = Dense(
-        tfp.layers.MultivariateNormalTriL.params_size(latent_dim),
-        activation=None,
+        tfp.layers.MultivariateNormalTriL.params_size(latent_dim), activation=None
     )(h)
 
-    return Model(input_layer, h, name="encoder")
+    return Model(input_layer, h)
 
 
 def create_decoder(
@@ -77,7 +79,10 @@ def create_decoder(
     latent_dim,
     filters,
     kernels,
+    conv_activation=None,
+    dense_activation=None,
 ):
+
     """
     Create the decoder of VAE model
 
@@ -86,19 +91,22 @@ def create_decoder(
         latent_dim: size of the latent space
         filters: filters used for the convolutional layers
         kernels: kernels used for the convolutional layers
+        conv_activation: activation for conv layers default PReLU
+        desnse_activation: activation for dense layers default PReLU
     """
     input_layer = Input(shape=(latent_dim,))
-    h = Dense(256)(input_layer)
+    h = PReLU()(input_layer)
+    h = Dense(tfp.layers.MultivariateNormalTriL.params_size(32))(h)
     h = PReLU()(h)
     w = int(np.ceil(input_shape[0] / 2 ** (len(filters))))
-    h = Dense(w * w * filters[-1], activation=None)(tf.cast(h, tf.float32))
+    h = Dense(w * w * filters[-1], activation=dense_activation)(tf.cast(h, tf.float32))
     h = PReLU()(h)
     h = Reshape((w, w, filters[-1]))(h)
     for i in range(len(filters) - 1, -1, -1):
         h = Conv2DTranspose(
             filters[i],
             (kernels[i], kernels[i]),
-            activation=None,
+            activation=conv_activation,
             padding="same",
             strides=(2, 2),
         )(h)
@@ -106,13 +114,12 @@ def create_decoder(
         h = Conv2DTranspose(
             filters[i],
             (kernels[i], kernels[i]),
-            activation=None,
+            activation=conv_activation,
             padding="same",
         )(h)
         h = PReLU()(h)
-
-    # keep the output of the last layer as relu as we want only positive flux values.
-    h = Conv2D(input_shape[-1] * 2, (3, 3), activation="relu", padding="same")(h)
+        
+    h = Conv2D(input_shape[-1]*2, (3, 3), activation="relu", padding="same")(h)
 
     # In case the last convolutional layer does not provide an image of the size of the input image, cropp it.
     cropping = int(h.get_shape()[1] - input_shape[0])
@@ -126,21 +133,20 @@ def create_decoder(
             )(h)
 
     # Build the encoder only
-    h = tfp.layers.DistributionLambda(
-        make_distribution_fn=lambda t: tfd.Normal(
-            loc=t[..., : input_shape[-1]], scale=1e-3 + t[..., input_shape[-1] :]
-        ),
-        convert_to_tensor_fn=tfp.distributions.Distribution.sample,
-    )(h)
-
-    return Model(input_layer, h, name="decoder")
-
+    h = tfp.layers.DistributionLambda(make_distribution_fn=lambda t: tfd.Normal(loc=t[...,:input_shape[-1]], scale=1e-4 +t[...,input_shape[-1]:])
+                                          ,convert_to_tensor_fn=tfp.distributions.Distribution.sample)(h)
+    
+    
+    
+    return Model(input_layer,h)
 
 def create_model_vae(
     input_shape,
     latent_dim,
     filters,
     kernels,
+    conv_activation=None,
+    dense_activation=None,
 ):
     """
     Create the VAE model
@@ -150,22 +156,28 @@ def create_model_vae(
         latent_dim: size of the latent space
         filters: filters used for the convolutional layers
         kernels: kernels used for the convolutional layers
+        conv_activation: activation for conv layers default PReLU
+        desnse_activation: activation for dense layers default PReLU
     """
-
+    
     encoder = create_encoder(
-        input_shape,
-        latent_dim,
-        filters,
-        kernels,
-    )
-
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+)
+    
     decoder = create_decoder(
-        input_shape,
-        latent_dim,
-        filters,
-        kernels,
-    )
-
+    input_shape,
+    latent_dim,
+    filters,
+    kernels,
+    conv_activation=None,
+    dense_activation=None,
+)
+    
     # Define the prior for the latent space
     prior = tfd.Independent(
         tfd.Normal(loc=tf.zeros(latent_dim), scale=1), reinterpreted_batch_ndims=1
@@ -176,13 +188,11 @@ def create_model_vae(
     z = tfp.layers.MultivariateNormalTriL(
         latent_dim,
         activity_regularizer=tfp.layers.KLDivergenceRegularizer(prior, weight=0.01),
-        name="latent_space",
     )(encoder(x_input))
-
-    net = Model(inputs=x_input, outputs=decoder(z))
-
+    
+    net = Model(inputs = x_input, outputs = decoder(z))
+    
     return net, encoder, decoder, Model(inputs=x_input, outputs=z)
-
 
 def load_deblender(
     input_shape,
